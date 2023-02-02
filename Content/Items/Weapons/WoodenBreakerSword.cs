8377e8a5a13e8ef2029b18f011f413558fdf6788
Way <89326159+WayDougwas@users.noreply.github.com>
2023-02-02 19:44:26 -0300

    Modificaçoes em dano e novo sprite

---
+++ Content/Items/Weapons/WoodenBreakerSword.cs
@@ -0,0 +1,59 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace Woodenbreaker.Content.Items.Weapons
+{
+    class WoodenBreakerSword : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            //Texts
+            DisplayName.SetDefault("Wooden Breaker Sword");
+            Tooltip.SetDefault("This is a legendary sword made of wood\nHis power is infinity");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            //Scale
+            Item.width = 48;
+            Item.height = 56;
+
+            //Animation
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+
+            //Damage
+            Item.DamageType = DamageClass.Melee;
+            Item.damage = 121;
+            Item.knockBack = 5;
+            Item.crit = 20;
+            Item.autoReuse = true;
+
+            //Misc
+            Item.value = Item.buyPrice(gold: 30);
+            Item.rare = ItemRarityID.Yellow;
+
+            //Sound
+            Item.UseSound = SoundID.Item1;
+
+
+        }
+        public override void AddRecipes()
+        {
+            //Craft
+            CreateRecipe()
+                .AddIngredient(ItemID.BrokenHeroSword)
+                .AddIngredient(ItemID.ChlorophyteBar, 25)
+                .AddIngredient(ItemID.Wood, 300)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+
+
+        }
+    }
+}
---
+++ Woodenbreaker.cs
@@ -0,0 +1,9 @@
+using Terraria.ModLoader;
+
+namespace Woodenbreaker
+{
+	public class Woodenbreaker : Mod
+	{
+		public const string ASSET_PATH = "WoodenBreaker/Assets/";
+	}
+}
